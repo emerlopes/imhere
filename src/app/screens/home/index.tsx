9b4e60f14379fe90fb94d1351d2f8d832acb05f7
Emerson <lopes.ds@live.com>
2024-10-08 23:21:02 -0300

    chore: add participant

--- src/app/screens/home/index.tsx
+++ src/app/screens/home/index.tsx
@@ -4,11 +4,17 @@ import { styles } from '@/app/screens/home/styles';
 import Input from '@/components/input';
 import Button from '@/components/button';
 import Participant from '@/components/participant';
+import { useState } from 'react';
 
 export default function Home() {
 
+    const [participantName, setParticipantName] = useState('');
+    const [participantsList, setParticipantsList] = useState<string[]>([]);
+
     function handleParticipantAdd() {
-        console.log("Participante adicionado")
+        setParticipantsList([...participantsList, participantName]);
+        console.log("Participante adicionado:", participantName);
+        setParticipantName('');
     }
 
     return (
@@ -17,13 +23,16 @@ export default function Home() {
             <Text style={styles.eventDate}>Segunda, 7 de Outubro de 2024</Text>
 
             <View style={styles.form}>
-                <Input />
+                <Input 
+                    value={participantName} 
+                    onChangeText={setParticipantName} 
+                />
                 <Button title="+" onPress={handleParticipantAdd} />
             </View>
 
-            <Participant name="Emerson Lopes"/>
-            <Participant name="Emerson Lopes"/>
-
+            {participantsList.map((name, index) => (
+                <Participant key={index} name={name} />
+            ))}
         </View>
     );
 }
--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,13 +1,14 @@
-import { TextInput } from "react-native"
+import { TextInput, TextInputProps } from "react-native"
 
 import { styles } from "@/components/input/styles";
 
-export default function Input() {
+export default function Input({ ...rest }: TextInputProps) {
     return (
         <TextInput
             style={styles.container}
             placeholder="Nome do participante"
             placeholderTextColor="#6B6B6B"
+            {...rest}
         />
     )
 }
