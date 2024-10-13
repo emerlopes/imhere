a28f09825210b6b24dae4fdb1be9260cc60ae026
Emerson <lopes.ds@live.com>
2024-10-13 18:41:51 -0300

    chore: implementando status bar

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import Home from '@/app/screens/home/index';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <>
+      <StatusBar backgroundColor="transparent" translucent/>
       <Home />
     </>
   );
