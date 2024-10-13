94ecf2437e645332e5ed71e648774fc35c038943
Emerson <lopes.ds@live.com>
2024-10-13 18:42:11 -0300

    chore: implementando validacao de participante já adicionado.

--- src/app/screens/home/index.tsx
+++ src/app/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, Text, View } from 'react-native';
+import { Alert, FlatList, Text, View } from 'react-native';
 
 import { styles } from '@/app/screens/home/styles';
 import Input from '@/components/input';
@@ -12,14 +12,29 @@ export default function Home() {
     const [participantsList, setParticipantsList] = useState<string[]>([]);
 
     function handleParticipantAdd() {
-        setParticipantsList([...participantsList, participantName]);
+
+        const name = participantName.trim()
+
+        if (participantsList.includes(name)) {
+            return Alert.alert("Participante", `O participante ${name} já está participando do evento`)
+        }
+
+        setParticipantsList(prevState => [...prevState, participantName]);
         console.log("Participante adicionado:", participantName);
         setParticipantName('');
     }
 
     function handleParticipantRemove(name: string) {
-        console.log("Removendo o participante " + name + " da lista")
-        setParticipantsList(participantsList.filter(participant => participant !== name))
+        Alert.alert("Participante", `Deseja remover o participante ${name}?`, [
+            {
+                text: "Sim",
+                onPress: () => setParticipantsList(participantsList.filter(participant => participant !== name))
+            },
+            {
+                text: "Não",
+                style: "cancel"
+            }
+        ])
     }
 
     return (
